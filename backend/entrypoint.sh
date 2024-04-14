a4a68a306963b3e51cf65950ac0cee91c5485aa7
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-04-14 13:34:56 -0300

    feat: adding entrypoint.sh and updating Dockerfile

---
+++ backend/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+poetry run python manage.py makemigrations
+poetry run python manage.py migrate
+
+exec "$@"
