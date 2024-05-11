5f934cb23c27e0874c16dc04a815ee3f1315a628
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-05-11 18:46:28 -0300

    feat: Add API endpoint for historical predict using linear regression algorithm

--- backend/project/station/urls.py
+++ backend/project/station/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path, re_path
+from django.urls import path
+
 from . import views
 
 app_name = 'station'
 
 urlpatterns = [
-    path('historical/predict/data/', views.get_historical_predict, name="historical_predict"),
+    # path('historical/predict/data/', views.get_historical_predict, name="historical_predict"),
+    path('historical/predict/linear-regression/ibirapuera-park/', views.get_historical_predict_linear_regression_ibirapuera_park,
+         name="historical_predict_linear_regression"),
 ]
