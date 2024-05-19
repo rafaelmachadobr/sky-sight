d912186c09ddfadd0c8de2009f096b131dc7f1f2
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-05-19 12:03:27 -0300

    refactor: Update weather_alert function to return a list of alerts

--- backend/project/station/utils.py
+++ backend/project/station/utils.py
@@ -5,6 +5,7 @@ import pickle
 from datetime import datetime
 from math import floor
 from pathlib import Path
+from typing import List
 
 import numpy as np
 import requests
@@ -122,7 +123,7 @@ def __get_condition_tip(temperature: int) -> tuple:
     return "Quente", "Mantenha-se fresco e hidratado durante o dia."
 
 
-def __weather_alert(temperature: int) -> str:
+def __weather_alert(temperature: int) -> List[str]:
     """
     Fornece um alerta climático com base na temperatura e umidade.
 
@@ -137,25 +138,27 @@ def __weather_alert(temperature: int) -> str:
     if temperature > 30:
         alerts.append("Proteja-se do Sol e do Calor:")
         alerts.extend([
-            "Evite a exposição direta ao sol das 10h às 16h."
-            "Use protetor solar para evitar queimaduras solares e danos à pele."
-            "Utilize chapéus e óculos escuros, principálmente se tiver pele clara."
-            "Proteja as crianças com chapéus de abas e roupas leves."
-            "Vista roupas leves e que não retenham calor."
-            "Diminua os esforços físicos e descanse frequentemente em locais com sombra fresca."
-            "Em veículos sem ar-condicionado, mantenha as janelas abertas para a ventilação."
-            "Nunca deixe crianças e animais dentro de veículos estacionados, mesmo que por curto período."
+            "Evite a exposição direta ao sol das 10h às 16h.",
+            "Use protetor solar para evitar queimaduras solares e danos à pele.",
+            "Utilize chapéus e óculos escuros, principalmente se tiver pele clara.",
+            "Proteja as crianças com chapéus de abas e roupas leves.",
+            "Vista roupas leves e que não retenham calor.",
+            "Diminua os esforços físicos e descanse frequentemente em locais com sombra fresca.",
+            "Em veículos sem ar-condicionado, mantenha as janelas abertas para a ventilação.",
+            "Nunca deixe crianças e animais dentro de veículos estacionados, " +
+            "mesmo que por curto período."
         ])
     if temperature >= 25:
         alerts.append("Hidratação é fundamental:")
         alerts.extend([
-            "Aumente a ingestão de água ou de sucos de frutas naturais, sem adição de açúcar."
-            "Evite bebidas alcoólicas e com elevado teor de açúcar."
-            "Faça refeições leves, pouco condimentadas e mais frequentes."
-            "Recém-nascidos, crianças, idosos e pessoas doentes podem não sentir sede. Ofereça água sempre."
+            "Aumente a ingestão de água ou de sucos de frutas naturais, sem adição de açúcar.",
+            "Evite bebidas alcoólicas e com elevado teor de açúcar.",
+            "Faça refeições leves, pouco condimentadas e mais frequentes.",
+            "Recém-nascidos, crianças, idosos e pessoas doentes podem não sentir sede. " +
+            "Ofereça água sempre."
         ])
 
-    return alerts if alerts else "Sem alertas no momento."
+    return alerts if alerts else ["Sem alertas no momento."]
 
 
 def __get_model_input(data: tuple, mean_pressure_inst: int) -> list:
